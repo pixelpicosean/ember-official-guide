6b48729986c0d53aed8b9c9d86f92348f544edad
Sean <pixelpicosean@gmail.com>
2014-04-16 07:47:23 +0800

    Add model fixtures

--- js/app.js
+++ js/app.js
@@ -2,15 +2,36 @@ window.addEventListener('load', function() { 'use strict';
 
     var Todos = window.Todos = Ember.Application.create();
 
+    // Routes -----------------------------------------------
     Todos.Router.map(function() {
         this.resource('todos', { path: '/' });
     });
 
     Todos.IndexRoute = Ember.Route.extend({});
 
+    // Model ------------------------------------------------
     Todos.Todo = DS.Model.extend({
         title: DS.attr('string'),
         isCompleted: DS.attr('boolean')
     });
+
+    // Fixtures ---------------------------------------------
+    Todos.Todo.FIXTRES = [
+        {
+            id: 1,
+            title: 'Learn Ember.js',
+            isCompleted: true
+        },
+        {
+            id: 2,
+            title: '...',
+            isCompleted: false
+        },
+        {
+            id: 3,
+            title: 'Profit!',
+            isCompleted: false
+        }
+    ];
 
 });
