f1f393f5fbe262728dcf34006c8b90d84c023b04
Sean <pixelpicosean@gmail.com>
2014-04-17 10:20:49 +0800

    Transition to show only completed todos

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ window.addEventListener('load', function() { 'use strict';
             // Add this function to disable default index
             // routes which enables "TodosIndexRoute"
             this.route('active');
+            this.route('completed');
         });
     });
 
@@ -28,6 +29,19 @@ window.addEventListener('load', function() { 'use strict';
         model: function() {
             return this.store.filter('todo', function(todo) {
                 return !todo.get('isCompleted');
+            });
+        },
+        renderTemplate: function(controller) {
+            this.render('todos/index', {
+                controller: controller
+            });
+        }
+    });
+
+    Todos.TodosCompletedRoute = Ember.Route.extend({
+        model: function() {
+            return this.store.filter('todo', function(todo) {
+                return todo.get('isCompleted');
             });
         },
         renderTemplate: function(controller) {
