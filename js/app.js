0771ab1d8e6bda5b13e6982858f869d01babaef7
Sean <pixelpicosean@gmail.com>
2014-04-17 14:23:49 +0800

    Remote unused fixture data

--- js/app.js
+++ js/app.js
@@ -158,23 +158,4 @@ window.addEventListener('load', function() { 'use strict';
         }
     });
 
-    // Fixtures ---------------------------------------------
-    Todos.Todo.FIXTURES = [
-        {
-            id: 1,
-            title: 'Learn Ember.js',
-            isCompleted: true
-        },
-        {
-            id: 2,
-            title: '...',
-            isCompleted: false
-        },
-        {
-            id: 3,
-            title: 'Profit!',
-            isCompleted: false
-        }
-    ];
-
 });
